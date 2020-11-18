ecdcfec8b6c6a8b06519002d44b9762da55704ff
JustGinger888 <ethanmoffatt888@icloud.com>
2020-11-18 23:37:13 +0000

    frontend

---
+++ frontend/src/components/Messages.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 class="display-1">Messages</h1>
+
+    <v-list dense>
+      <v-list-item-group
+        v-model="selectedItem"
+        color="primary"
+      >
+        <v-list-item
+          v-for="message in messages"
+          :key="message"
+          v-on:click="c"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="message"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      messages: ["Hello", "Hi"]
+    }
+  }
+}
+</script>
